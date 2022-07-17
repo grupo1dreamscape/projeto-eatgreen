b19cd5bf515d935c62729291d02bab9672bf89d3
Angela <commercial.araujo@gmail.com>
2022-07-17 14:11:02 -0300

    store-block-template

---
+++ store-block-template/react/Countdown.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
+
+
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+
+interface CountdownProps {
+  targetDate: string
+}
+
+const CSS_HANDLES = ['countdown' ]
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  targetDate = DEFAULT_TARGET_DATE }) => {
+    const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  });
+
+  
+  const handles = useCssHandles(CSS_HANDLES)
+  
+  tick(targetDate, setTime) // função que atualiza o nosso estado a cada um segundo
+
+  return (
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+    
+  );
+};
+  
+Countdown.schema = {
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    }
+  },
+}
+
+export default Countdown
